26065db864c8c631f65ffc27ee17264bebf69080
TodV <d2tod12@gmail.com>
2024-10-20 17:14:06 -0600

    Creacion del controlador (falta codigo)
    Se implementaron los metodos abtractos

---
+++ controlador/Controlador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import Interfaces.Observador;
+
+/**
+ *
+ * @author d2tod
+ */
+public class Controlador implements Observador {
+
+    @Override
+    public void actulizar(Object señal) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
